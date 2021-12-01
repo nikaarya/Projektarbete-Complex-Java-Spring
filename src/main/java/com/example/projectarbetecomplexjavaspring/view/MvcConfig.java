662fd62847e13739f885b0724a61938c82ad806e
nikaarya <nik.ary94@gmail.com>
2021-12-01 15:22:44 +0100

    Added packages

---
+++ src/main/java/com/example/projectarbetecomplexjavaspring/view/MvcConfig.java
@@ -0,0 +1,6 @@
+package com.example.projectarbetecomplexjavaspring.view;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfig implements WebMvcConfigurer {
+}
