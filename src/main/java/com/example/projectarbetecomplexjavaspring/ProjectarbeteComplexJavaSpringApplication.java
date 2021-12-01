f63c3429868be3bce5119a344f819f45d69b5278
nikaarya <nik.ary94@gmail.com>
2021-12-01 15:25:36 +0100

    Added packages

--- src/main/java/com/example/projectarbetecomplexjavaspring/ProjectarbeteComplexJavaSpringApplication.java
+++ src/main/java/com/example/projectarbetecomplexjavaspring/ProjectarbeteComplexJavaSpringApplication.java
@@ -9,6 +9,7 @@ public class ProjectarbeteComplexJavaSpringApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectarbeteComplexJavaSpringApplication.class, args);
 
+
     }
 
 }
