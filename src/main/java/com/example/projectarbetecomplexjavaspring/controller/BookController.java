d10c4d28bc13304375396dc5234406ebf65e18e1
nikaarya <nik.ary94@gmail.com>
2021-12-02 14:22:00 +0100

    Created and filled in all controller classes

--- src/main/java/com/example/projectarbetecomplexjavaspring/controller/BookController.java
+++ src/main/java/com/example/projectarbetecomplexjavaspring/controller/BookController.java
@@ -1,4 +1,38 @@
 package com.example.projectarbetecomplexjavaspring.controller;
 
+import com.example.projectarbetecomplexjavaspring.entity.BookEntity;
+import com.example.projectarbetecomplexjavaspring.entity.StudentEntity;
+import com.example.projectarbetecomplexjavaspring.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("books")
 public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<BookEntity> createBook(@RequestBody BookEntity bookEntity) {
+        BookEntity createdBook = bookService.createBook(bookEntity);
+        return new ResponseEntity<>(createdBook, HttpStatus.CREATED);
+    }
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Optional<BookEntity>> findBookById(@PathVariable Long id) {
+        Optional<BookEntity> foundBook = bookService.findBookById(id);
+        return new ResponseEntity<>(foundBook, HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/projectarbetecomplexjavaspring/controller/SubjectController.java
+++ src/main/java/com/example/projectarbetecomplexjavaspring/controller/SubjectController.java
@@ -1,4 +1,36 @@
 package com.example.projectarbetecomplexjavaspring.controller;
 
+import com.example.projectarbetecomplexjavaspring.entity.SubjectEntity;
+import com.example.projectarbetecomplexjavaspring.entity.TeacherEntity;
+import com.example.projectarbetecomplexjavaspring.service.SubjectService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("subjects")
 public class SubjectController {
+    private final SubjectService subjectService;
+
+    public SubjectController(SubjectService subjectService) {
+        this.subjectService = subjectService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<SubjectEntity> createSubject(@RequestBody SubjectEntity subjectEntity) {
+        SubjectEntity createdSubject = subjectService.createSubject(subjectEntity);
+        return new ResponseEntity<>(createdSubject, HttpStatus.CREATED);
+    }
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteSubject(@PathVariable Long id) {
+        subjectService.deleteSubject(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping()
+    public ResponseEntity<Iterable<SubjectEntity>> findAllSubjects() {
+        Iterable<SubjectEntity> allSubjects = subjectService.findAllSubjects();
+        return new ResponseEntity<>(allSubjects, HttpStatus.OK);
+    }
+
 }
