35441bab299c6f5d5c324a813bd0464c91480ec4
nikaarya <nik.ary94@gmail.com>
2021-12-02 11:57:19 +0100

    Filled in controller class for Teacher

--- src/main/java/com/example/projectarbetecomplexjavaspring/controller/TeacherController.java
+++ src/main/java/com/example/projectarbetecomplexjavaspring/controller/TeacherController.java
@@ -1,4 +1,39 @@
 package com.example.projectarbetecomplexjavaspring.controller;
 
+import com.example.projectarbetecomplexjavaspring.entity.TeacherEntity;
+import com.example.projectarbetecomplexjavaspring.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("teachers")
 public class TeacherController {
+    //@Autowired
+    private final TeacherService teacherService;
+
+    public TeacherController(TeacherService teacherService) {
+        this.teacherService = teacherService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<TeacherEntity> createTeacher(@RequestBody TeacherEntity teacherEntity) {
+        TeacherEntity createdTeacher = teacherService.createTeacher(teacherEntity);
+        return new ResponseEntity<>(createdTeacher, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteTeacher(@PathVariable Long id) {
+        teacherService.deleteTeacher(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Optional<TeacherEntity>> findTeacherById(@PathVariable Long id) {
+        Optional<TeacherEntity> foundTeacher = teacherService.findTeacherById(id);
+        return new ResponseEntity<>(foundTeacher, HttpStatus.OK);
+    }
 }
